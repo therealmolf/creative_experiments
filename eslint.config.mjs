01de9afccffd4ca8a34bc8c6f0b5e43de2a126a7
Miko Planas <miksbon@gmail.com>
2025-10-17 18:32:24 +0800

    Add common p5 functions to globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,50 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        background: "readonly",
+        createCanvas: "readonly",
+        draw: "readonly",
+        ellipse: "readonly",
+        fill: "readonly",
+        height: "readonly",
+        image: "readonly",
+        line: "readonly",
+        loadImage: "readonly",
+        loadSound: "readonly",
+        mount: "readonly",
+        noFill: "readonly",
+        noStroke: "readonly",
+        noTint: "readonly",
+        p5: "readonly",
+        pop: "readonly",
+        push: "readonly",
+        rect: "readonly",
+        rotate: "readonly",
+        setup: "readonly",
+        soundFormats: "readonly",
+        stroke: "readonly",
+        text: "readonly",
+        textAlign: "readonly",
+        textSize: "readonly",
+        tint: "readonly",
+        translate: "readonly",
+        width: "readonly",
+      },
+    },
+  },
+  {
+    files: ["sketches/**/*.{js,mjs,cjs,ts,mts,cts}"],
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off"
+    }
+  },
 ]);
