5bf83d1a2a9f43a79af6f11283ad1de728a91f5f
Miko Planas <miksbon@gmail.com>
2025-11-03 13:18:10 +0800

    Add pixel manipulation to maps.png in trains sketch

--- sketches/trains/sketch.js
+++ sketches/trains/sketch.js
@@ -1,51 +1,39 @@
 let img;
-const GRID_SIZE = 5;
-const CELL_SIZE = 160;
+let pxCol;
 
 function preload() {
-  img = loadImage("../../assets/images/flowy_amber_glass_textures_mixed.png");
+  // Load any assets here if needed
+  img = loadImage("../../assets/images/Maps.png");
 }
 
 function setup() {
-  createCanvas(GRID_SIZE * CELL_SIZE, GRID_SIZE * CELL_SIZE);
+  createCanvas(800, 800);
   noLoop();
 }
 
 function draw() {
-  background(220);
+  // background(212, 192, 160);
+  background(255);
 
-  for (let i = 0; i < GRID_SIZE; i++) {
-    for (let j = 0; j < GRID_SIZE; j++) {
-      push();
-      translate(i * CELL_SIZE + CELL_SIZE / 2, j * CELL_SIZE + CELL_SIZE / 2);
+  image(img);
+  img.resize(800, 800);
 
-      // Random rotation
-      const angle = random([0, 90, 180, 270]);
-      rotate(radians(angle));
+  noStroke();
 
-      // Random reflection
-      const reflectX = random([-1, 1]);
-      const reflectY = random([-1, 1]);
-      scale(reflectX, reflectY);
-
-      // Random square snapshot from the image
-      const snapshotSize = int(random(50, 150));
-      const sx = int(random(0, img.width - snapshotSize));
-      const sy = int(random(0, img.height - snapshotSize));
-
-      image(
-        img,
-        -CELL_SIZE / 2,
-        -CELL_SIZE / 2,
-        CELL_SIZE,
-        CELL_SIZE,
-        sx,
-        sy,
-        snapshotSize,
-        snapshotSize
-      );
-
-      pop();
+  //Pixelation Loop
+  for (var recY = 0; recY < height; recY += 40) {
+    for (var recX = 0; recX < width; recX += 10) {
+      pxCol = img.get(recX, recY);
+      fill(pxCol);
+      rect(recX, recY, 15, 10);
     }
   }
+
+  // Add brown coffee grain layer
+  // for (let i = 0; i < 10000; i++) {
+  //   let x = random(width);
+  //   let y = random(height);
+  //   stroke(101, 67, 33, 100); // Brown color with some transparency
+  //   point(x, y);
+  // }
 }
